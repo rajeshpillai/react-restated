f57f67826b3f14322d46d2005ac3c403f89fee6d
rajesh.pillai <rajesh.pillai@kohls.com>
2018-03-19 11:06:39 -0700

    Refactored: Removed unwanted code

--- src/ReStated/index.js
+++ src/ReStated/index.js
@@ -4,31 +4,13 @@ import React, { Component } from 'react';
 export const StateContext = React.createContext(null);
 export const {Provider, Consumer} = StateContext;
 
-const SKIP_PROPERTIES = ["props", "context", "setState", "forceUpdate",
-                          "refs", "updater",
-                          "_reactInternalFiber","_reactInternalInstance",
-                          "isReactComponent"];
-
 export class Container extends Component {
     constructor() {
-        console.log("Container:ctor");
         super();
         this.setup = this.setup.bind(this);
-        this.setupDone = false;
-        this.map = {};
     }
     
     setup() {
-        // let map = {};
-        // // todo: This code may be refactored
-        // for (let property in this) {
-        //     if (property.startsWith("_")) continue;  // SKIP private methods, starts with "_"
-        //     if (SKIP_PROPERTIES.indexOf(property) < 0 ) {
-        //         console.log(`Setting ${property}`)
-        //         map[property] =  this[property];
-        //     }
-        // }
-        // return map;
         let map = {};
         map.state = this.state;
         map.actions = this.actions;
