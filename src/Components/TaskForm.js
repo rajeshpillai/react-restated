8da070cf469003d384f1be4e2ed7b2d73888d181
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-11-27 14:57:07 +0530

    FIX: ref (now uses React.createRef)

--- src/Components/TaskForm.js
+++ src/Components/TaskForm.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import {Consumer } from '../ReStated';
 
-const TaskForm = () => (
-    <Consumer>
+const TaskForm = () => {
+    let taskTitle = React.createRef();
+    return <Consumer>
       {(context) => (
         <div>
-            <input className="input-title" ref={(title)=>{this.taskTitle = title}} 
+            <input className="input-title" ref={taskTitle} refxxx={(title)=>{this.taskTitle = title}} 
               type="text" placeholder="what do you want to do today?" />
             <button className="button-add" type="submit"
-              onClick={(e) => {context.actions.onAddTask(this.taskTitle.value)}}>
+              onClick={(e) => {context.actions.onAddTask(taskTitle.current.value)}}>
               &#x271A;
             </button>
         </div>
       )}
     </Consumer>
-)
+}
 
 export default TaskForm;
   
